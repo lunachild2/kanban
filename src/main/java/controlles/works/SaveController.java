1b1c7f3bd5ad509e0e7342f5d264d886fb89d94b
lunachild2 <gkgktjddus@gmail.com>
2023-08-04 21:50:49 +0900

    추가 수정

---
+++ src/main/java/controlles/works/SaveController.java
@@ -0,0 +1,17 @@
+package controlles.works;
+
+import controlles.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SaveController implements Controller {
+    @Override
+    public void get(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+
+    @Override
+    public void post(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+}
