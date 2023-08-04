da93ba7ca74b5e2102b1eccef998f999f52e5cd6
lunachild2 <gkgktjddus@gmail.com>
2023-08-04 21:45:41 +0900

    추가 수정

---
+++ src/main/java/controlles/Controller.java
@@ -0,0 +1,12 @@
+package controlles;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface Controller {
+
+    void get(HttpServletRequest req, HttpServletResponse resp);
+
+    void post(HttpServletRequest req, HttpServletResponse resp);
+
+}
---
+++ src/main/java/controlles/works/WorksController.java
@@ -0,0 +1,27 @@
+package controlles.works;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/works/*")
+public class WorksController extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+
+    }
+}
