32c57e34dda59f9966c700a4777b34005699646e
lunachild2 <gkgktjddus@gmail.com>
2023-08-07 20:46:17 +0900

    아이디 찾기 추가

---
+++ src/main/java/controlles/member/FindIdController.java
@@ -0,0 +1,18 @@
+package controlles.member;
+
+import commons.ViewUtils;
+import controlles.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FindIdController implements Controller {
+    @Override
+    public void get(HttpServletRequest req, HttpServletResponse resp) {
+        ViewUtils.load(req, resp, "member", "find_id");
+    }
+
+    @Override
+    public void post(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+}
