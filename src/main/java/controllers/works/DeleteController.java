cfdff4f1fa18043d7cd221fd5b53a79b47d20a61
lunachild2 <gkgktjddus@gmail.com>
2023-08-09 21:14:18 +0900

    코드 리팩토링

--- src/main/java/controllers/works/DeleteController.java
+++ src/main/java/controllers/works/DeleteController.java
@@ -1,17 +1,36 @@
 package controllers.works;
 
+import commons.UrlUtils;
 import controllers.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import models.works.InfoService;
+import models.works.Work;
+import models.works.WorkServiceManager;
+
+import static commons.ScriptUtils.alertError;
 
 public class DeleteController implements Controller {
     @Override
     public void get(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            InfoService infoService = WorkServiceManager.getInstance().infoService();
+            long workNo = UrlUtils.getPatternData(req, "edit/(\\d*)");
 
+
+        } catch (Exception e) {
+            alertError(resp, e, -1);
+        }
     }
 
     @Override
     public void post(HttpServletRequest req, HttpServletResponse resp) {
 
+    }
+
+    private long getWorkNo(HttpServletRequest req) {
+
+
+        return 0L;
     }
 }
