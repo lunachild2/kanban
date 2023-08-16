dcf0dc7e34f3f4a6b1477ecdb2c85f65f380fd4e
lunachild2 <gkgktjddus@gmail.com>
2023-08-16 12:39:17 +0900

    코드 추가 및 수정

--- src/main/java/models/member/LoginService.java
+++ src/main/java/models/member/LoginService.java
@@ -1,6 +1,8 @@
 package models.member;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 public class LoginService {
@@ -13,7 +15,7 @@ public class LoginService {
         this.usersDao = usersDao;
     }
 
-    public void login(HttpServletRequest req) {
+    public void login(HttpServletRequest req, HttpServletResponse resp) {
 
         validator.Check(req);
 
@@ -22,5 +24,18 @@ public class LoginService {
         String userId = req.getParameter("userId");
         Users users = usersDao.get(userId);
         session.setAttribute("users", users);
+
+        // 아이디 저장 처리
+        Cookie cookie = new Cookie("saveId", userId);
+        cookie.setHttpOnly(true); // 스크립트에서 조회 X, 보안 O
+        if (req.getParameter("saveId") == null) { // 아이디 저장 미체크
+            // 기존 저장 쿠키 삭제
+            cookie.setMaxAge(0);
+        } else { // 아이디 저장 체크 상태
+            // 쿠키 저장
+            cookie.setMaxAge(60 * 60 * 24 * 365);
+        }
+        resp.addCookie(cookie);
+
     }
 }
